92981b87520a1798153c851c381a5a0e05ad4a34
ds12a <81164854+ds12a@users.noreply.github.com>
2023-11-25 12:08:48 -0500

    show velocities

--- src/components/Diagram.jsx
+++ src/components/Diagram.jsx
@@ -26,20 +26,24 @@ export default function Diagram({ passedPlotInfos, detail }) {
         Array.from({ length: len }, (_, i) => Number(start) + (Number(stop) - Number(start)) / len * i);
     const mask = (len) =>
         Array.from({ length: len }, (_, i) => (i === 0 ? 12 : 0));
+    const text = (len, value) =>
+        Array.from({ length: len }, (_, i) => (i === 0 ? "" : value));
 
     var markerMask = [];
-
+    var velocities = [];
 
     var modified = structuredClone(plotInfos);
     for (var i = 0; i < plotInfos.length; i++) {
         var pi = plotInfos[i];
         var newPlotInfo = new PlotInfo([], [], []);
         markerMask.push([]);
+        velocities.push([]);
         for (var j = 0; j < pi.t.length - 1; j++) {
             newPlotInfo.x.push(...range(detail, pi.x[j], pi.x[j + 1]));
             newPlotInfo.y.push(...range(detail, pi.y[j], pi.y[j + 1]));
             newPlotInfo.t.push(...range(detail, pi.t[j], pi.t[j + 1]));
             markerMask[i].push(...mask(detail))
+            velocities[i].push(...text(detail, `Velocity: ${(Math.sqrt(Math.pow(pi.x[j + 1] - pi.x[j], 2) + Math.pow(pi.y[j + 1] - pi.y[j], 2)) / (pi.t[j + 1] - pi.t[j])).toFixed(3)} c`))
         }
         if (pi.t.length !== 0) {
             newPlotInfo.x.push(pi.x[pi.t.length - 1]);
@@ -61,12 +65,14 @@ export default function Diagram({ passedPlotInfos, detail }) {
                 type: 'scatter3d',
                 mode: 'lines+markers',
                 name: `Observer ${index}`,
-                marker: { size: markerMask.reduce((a, b) => a.length > b.length ? a : b) }
+                marker: { size: markerMask[index] },
+                text: velocities[index],
+                hovertemplate: 't = %{z}' +
+                    '<br>x = %{x}' +
+                    '<br>y = %{y}<br>' +
+                    '%{text}',
             }))
             }
-
-
-
             layout={layout}
         />
     );
